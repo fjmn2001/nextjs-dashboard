e3e69a6c7c78e4bd7897cbfd5ecf6a8fbd511656
Francisco Marcano <fjmn2001@gmail.com>
2025-02-22 16:59:43 -0400

    feat: finished course

---
+++ auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+      },
+    }),
+  ],
+});
